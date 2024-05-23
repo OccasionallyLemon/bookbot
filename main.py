523f610a0ac854255c6cfd179470f97dfc0de0fa
Adam <kethiel@msn.com>
2024-05-23 15:21:00 +0100

    Changed output to a report of word and letter counts sorted

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from string import ascii_lowercase
+
 path_to_file = "books/frankenstein.txt"
 
 def read_file(file_path):
@@ -10,9 +12,32 @@ def word_count(text):
     words = text.split()
     return len(words)
 
+def init_letters_array(letters):
+    for c in ascii_lowercase:
+        letters.append({'letter': c, 'count':0})
+    return letters
+
+def sort_on(dict):
+    return dict['count']
+
+def letters_count(text):
+    letters = []
+    init_letters_array(letters)
+    text = text.lower()
+    for a in text:
+        if a.isalpha():
+            for l in letters:
+                if l['letter'] == a:
+                    l['count'] +=1
+    letters.sort(reverse=True, key=sort_on)
+    return letters
 def main():
     text = read_file(path_to_file)
-    print(text)
+    print(f"--- Begin report of {path_to_file} ---")
     print(f"Word count: {word_count(text)}")
+    letters_counted = letters_count(text)
+    for l in letters_counted:
+       print(f"The letter {l['letter']} appears {l['count']} times")
+    print(f"--- End report ---")
 
 main()
